68b8f1c58d9ed57dc3a227dc8b4cdbbab0bd3625
Liwei <liliwei25@hotmail.com>
2017-11-10 03:05:18 +0800

    Updated user guides and PPP; removetag can not target single person; circular image

--- src/main/java/seedu/address/logic/parser/RemoveTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemoveTagCommandParser.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemoveTagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -12,6 +13,11 @@ import seedu.address.model.tag.Tag;
  * Parses input arguments and creates a new RemoveTagCommand object
  */
 public class RemoveTagCommandParser implements Parser<RemoveTagCommand> {
+
+    private static final String SPACE = " ";
+    private static final int TAG_POS = 1;
+    private static final int INDEX_POS = 0;
+
     /**
      * Parses the given {@code String} of arguments in the context of the RemoveTagCommand
      * and returns an RemoveTagCommand object for execution.
@@ -19,9 +25,15 @@ public class RemoveTagCommandParser implements Parser<RemoveTagCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public RemoveTagCommand parse(String arg) throws ParseException {
+        String[] splitArgs = arg.trim().split(SPACE);
         try {
-            Tag t = ParserUtil.parseTag(arg);
-            return new RemoveTagCommand(t);
+            if (splitArgs.length < 2) {
+                Tag t = ParserUtil.parseTag(splitArgs[INDEX_POS]);
+                return new RemoveTagCommand(RemoveTagCommand.ALL, t);
+            } else {
+                Tag t = ParserUtil.parseTag(splitArgs[TAG_POS]);
+                return new RemoveTagCommand(splitArgs[INDEX_POS], t);
+            }
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveTagCommand.MESSAGE_USAGE));
         }
--- src/test/java/seedu/address/logic/parser/RemoveTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemoveTagCommandParserTest.java
@@ -12,16 +12,23 @@ import seedu.address.model.tag.Tag;
 //@@author liliwei25
 public class RemoveTagCommandParserTest {
     private static final String VALID_INPUT = "test";
+    private static final String VALID_FIRST_INPUT = "1 test";
     private static final String INVALID_INPUT = "";
+    public static final String FIRST_INDEX = "1";
     private RemoveTagCommandParser parser = new RemoveTagCommandParser();
 
     @Test
-    public void parse_validArgs_returnsDeleteCommand() throws Exception {
-        assertParseSuccess(parser, VALID_INPUT, new RemoveTagCommand(new Tag(VALID_INPUT)));
+    public void parse_validArgs_returnsRemoveTagCommand() throws Exception {
+        // remove tag from all person
+        assertParseSuccess(parser, VALID_INPUT, new RemoveTagCommand(RemoveTagCommand.ALL, new Tag(VALID_INPUT)));
+
+        // remove tag from first person
+        assertParseSuccess(parser, VALID_FIRST_INPUT, new RemoveTagCommand(FIRST_INDEX, new Tag(VALID_INPUT)));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
+        // no input
         assertParseFailure(parser, INVALID_INPUT,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveTagCommand.MESSAGE_USAGE));
     }
